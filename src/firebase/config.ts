762c3b7025b8794e70483cef59e82e92c3a2e48e
DeniSetiawan <98098067+deni-stwn@users.noreply.github.com>
2024-12-17 09:11:28 +0700

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,12 @@ import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCV5CKtwNoKcb4Z5TdRadCHm-xkBacpXW4',
-  authDomain: 'sesi-12-76d8b.firebaseapp.com',
-  projectId: 'sesi-12-76d8b',
-  storageBucket: 'sesi-12-76d8b.firebasestorage.app',
-  messagingSenderId: '417241851179',
-  appId: '1:417241851179:web:f18a3fcaffb4af9380116e',
+  apiKey: '5',
+  authDomain: 'ses',
+  projectId: 'ses',
+  storageBucket: 'sesiapp',
+  messagingSenderId: '41179',
+  appId: '1:417241851180116e',
 };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
